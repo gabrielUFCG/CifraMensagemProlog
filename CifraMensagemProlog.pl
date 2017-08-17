0da3c2fa90c90d84ffdb531739de49f62b074f51
gabrielvba <gabriel.alves@ccc.ufcg.edu.br>
2017-08-16 22:09:20 -0300

    Cifragem completa

--- CifraMensagemProlog.pl
+++ CifraMensagemProlog.pl
@@ -7,7 +7,6 @@ insere(X, L, [X|L]).
 insereFim(T, [H], L):- insere(H, [T], L). 
 insereFim(N, [H|T], L):- insereFim(N, T, X), insere(H, X, L).
 
-
 colocaFlagEspaco(L, [] , []).
 colocaFlagEspaco(L, [L|T], ['w'|G]):- colocaFlagEspaco(L, T, G).
 colocaFlagEspaco(L, [H|T] , [H|G]):- colocaFlagEspaco(L, T, G).
@@ -23,8 +22,12 @@ colocaFlagFim(X, X, [H|T], [H|T]).
 colocaFlagFim(X, Y, [H|T], G) :- insereFim('k',[H|T], G).
 
 cifra2a2(C, C, [], MX,[]).
-cifra2a2(C, S, [H|T], MX , [R|M]) :-  head(T, HT), condicionaCifragem(H,HT,MX,R), tail(T,TT), N is C-1, cifra2a2(N,0,TT,MX,M).
+cifra2a2(C, S, [H|T], MX , Result) :-  head(T, HT), condicionaCifragem(H,HT,MX,R), tail(T,TT), N is C-1, cifra2a2(N,0,TT,MX,M), conc(R,M,Result).
 
+conc([],L,L).      		%  A lista vazia concatenada com qualquer lista resulta
+conc([X|L1],L2,[X|L3]):-	%  nesta propria lista.  A concatenação de duas listas
+conc(L1,L2,L3).   
+	
 getElement(Ind, [H|T], C):- Ind== 0 -> C = H; Z is Ind-1, getElement(Z, T, C).
 
 getInd(E, [], 1).
@@ -44,13 +47,12 @@ cifragem(A,B,C,D,MX,R) :- (A == C -> linha(A,B,C,D,MX,R); B == D -> coluna(A,B,C
 getElementNaMatriz(L,L,C,[H|T],E) :- getElement(C,H,E).
 getElementNaMatriz(L,X,C,[H|T],E) :- XX is X+1,getElementNaMatriz(L,XX,C,T,E).
 
-cons(A,B,[A|B]).
 
-linha(A,B,C,D,MX,R) :- (B == 4 -> BB = 0; BB = B+1),getElementNaMatriz(A,0,BB,MX,RA),(D == 4 ->DD = 0; DD = D+1),getElementNaMatriz(C,0,DD,MX,RB),cons(RA,RB,R).
+linha(A,B,C,D,MX,R) :- (B == 4 -> BB = 0; BB = B+1),getElementNaMatriz(A,0,BB,MX,RA),(D == 4 ->DD = 0; DD = D+1),getElementNaMatriz(C,0,DD,MX,RB),insereFim(RB,[RA],R).
 
-coluna(A,B,C,D,MX,R) :- (A == 4 -> AA = 0; AA is A+1),getElementNaMatriz(AA,0,B,MX,RA),(C == 4 ->CC = 0; CC is C+1),getElementNaMatriz(CC,0,D,MX,RB),cons(RA,RB,R).
+coluna(A,B,C,D,MX,R) :- (A == 4 -> AA = 0; AA is A+1),getElementNaMatriz(AA,0,B,MX,RA),(C == 4 ->CC = 0; CC is C+1),getElementNaMatriz(CC,0,D,MX,RB),insereFim(RB,[RA],R).
 
-retangulo(A,B,C,D,MX,R) :- getElementNaMatriz(C,0,B,MX,RA),getElementNaMatriz(A,0,D,MX,RB),cons(RA,RB,R).
+retangulo(A,B,C,D,MX,R) :- getElementNaMatriz(C,0,B,MX,RA),getElementNaMatriz(A,0,D,MX,RB),insereFim(RB,[RA],R).
 
 matriz([['a','b','c','d','e'],['f','g','h','i','j'], ['k','l','m','n','o'], ['p','q','r','s','t'], ['u','v','w','x','z']]).
 
