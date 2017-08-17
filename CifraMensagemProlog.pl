a2726a4de9affaffa8f33af7f28ccf913a0ee3b2
gabrielvba <gabriel.alves@ccc.ufcg.edu.br>
2017-08-16 21:39:39 -0300

    cifragem completa

--- CifraMensagemProlog.pl
+++ CifraMensagemProlog.pl
@@ -31,18 +31,28 @@ getInd(E, [], 1).
 getInd(E, [E|T], 0):-!.
 getInd(E, [H|T], Ind):- getInd(E, T, X), Ind is X+1.
 
-getColuna(E, [],  5).
-getColuna(E, [H|T],  R) :- getInd(E,H,X), getColuna(E,T,S), indiceReal(X,S,R) .
+getLinha(E, [], 6). 
+getLinha(E, [H|T], R) :- getInd(E,H,X), getLinha(E,T,S),(X<5 -> R = 0; R is 1+S) .
 
-indiceReal(X,X,X).
-indiceReal(X,Y,X). 
+getColuna(E, [], 6).
+getColuna(E, [H|T], R) :- getInd(E,H,X), getColuna(E,T,S),(X<5 -> R = X; R = S) .
 
-condicionaCifragem(L,H,MX,R) :- getColuna(L,MX,R). 
+condicionaCifragem(L,H,MX,R) :- getLinha(L,MX,A),getColuna(L,MX,B),getLinha(H,MX,C),getColuna(H,MX,D), cifragem(A,B,C,D,MX,R). 
+
+cifragem(A,B,C,D,MX,R) :- (A == C -> linha(A,B,C,D,MX,R); B == D -> coluna(A,B,C,D,MX,R); retangulo(A,B,C,D,MX,R)).
+
+getElementNaMatriz(L,L,C,[H|T],E) :- getElement(C,H,E).
+getElementNaMatriz(L,X,C,[H|T],E) :- XX is X+1,getElementNaMatriz(L,XX,C,T,E).
+
+cons(A,B,[A|B]).
+
+linha(A,B,C,D,MX,R) :- (B == 4 -> BB = 0; BB = B+1),getElementNaMatriz(A,0,BB,MX,RA),(D == 4 ->DD = 0; DD = D+1),getElementNaMatriz(C,0,DD,MX,RB),cons(RA,RB,R).
+
+coluna(A,B,C,D,MX,R) :- (A == 4 -> AA = 0; AA is A+1),getElementNaMatriz(AA,0,B,MX,RA),(C == 4 ->CC = 0; CC is C+1),getElementNaMatriz(CC,0,D,MX,RB),cons(RA,RB,R).
+
+retangulo(A,B,C,D,MX,R) :- getElementNaMatriz(C,0,B,MX,RA),getElementNaMatriz(A,0,D,MX,RB),cons(RA,RB,R).
 
 matriz([['a','b','c','d','e'],['f','g','h','i','j'], ['k','l','m','n','o'], ['p','q','r','s','t'], ['u','v','w','x','z']]).
-
-
-
 
 
 :- initialization main.
